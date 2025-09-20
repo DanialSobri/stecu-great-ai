d4b88edec0a649c3117a1a0011c78d051b6d8156
DanialSobri <adanial091@gmail.com>
2025-09-21 04:30:03 +0800

    Add Lambda function with Amplify backend configuration
    - Created my-first-function Lambda handler
    - Added backend.ts configuration
    - Set up Amplify function resource definition

---
+++ my-app/amplify/backend.ts
@@ -0,0 +1,6 @@
+import { defineBackend } from "@aws-amplify/backend";
+import { myFirstFunction } from './my-first-function/resource';
+
+export const backend = defineBackend({
+  myFirstFunction,
+});
